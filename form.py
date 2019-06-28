57b0faa918bec8d92c06fb2697d9b95fc8b7661c
Navid Bahadoran <navid.bahadoran@gmail.com>
2019-06-27 21:56:04 -0700

    form.py

--- form.py
+++ form.py
@@ -5,8 +5,8 @@ from model import Donor
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), length(min=2, max=20)])
-    name = StringField('Name', validators=[DataRequired(), length(min=2, max=20)])
+    username = StringField('Username', validators=[DataRequired(), length(min=2, max=40)])
+    name = StringField('Name', validators=[DataRequired(), length(min=2, max=40)])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
